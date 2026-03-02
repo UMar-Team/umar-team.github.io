59b270f164262f65b4f6773f1064d07b74373418
Gabriel Faria <gabriel.ldfaria@gmail.com>
2026-03-02 23:03:43 +0000

    chore: dependencies and public assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { useScrollAnimation } from "./hooks/useScrollAnimation";
 
 function App() {
+  const location = useLocation();
+  useScrollAnimation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
-    <div className="flex flex-col md:h-screen md:overflow-hidden max-md:min-h-screen text-tertiary bg-quaternary ">
+    <div className="flex flex-col max-md:min-h-screen text-tertiary bg-quaternary ">
       <Navbar />
-      <main className="flex-1 flex flex-col max-md:mt-20">
+      <main className="flex-1 flex flex-col">
         <Outlet />
       </main>
-      <div className="md:hidden"><Footer /></div>
+      <div ><Footer /></div>
     </div>
   );
 }
